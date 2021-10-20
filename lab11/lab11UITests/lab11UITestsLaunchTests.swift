8450f5e28e43dca84cd4572bb681f3f51feb57dc
grand ahmad <grand23@grands-MacBook-Pro.local>
2021-10-20 22:31:58 +0300

    workshop

---
+++ lab11/lab11UITests/lab11UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lab11UITestsLaunchTests.swift
+//  lab11UITests
+//
+//  Created by grand ahmad on 14/03/1443 AH.
+//
+
+import XCTest
+
+class lab11UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

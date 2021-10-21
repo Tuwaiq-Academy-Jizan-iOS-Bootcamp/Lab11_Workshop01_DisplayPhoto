8275103ba071b2c5b87fceb3927b09317c30908c
Doaa <doaa.ageel@gmail.com>
2021-10-21 09:14:18 +0300

    lab11

---
+++ Doaaalageel_Lab11_Workshop01_DisplayPhoto*/Doaaalageel_Lab11_Workshop01_DisplayPhoto*Tests/Doaaalageel_Lab11_Workshop01_DisplayPhoto_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Doaaalageel_Lab11_Workshop01_DisplayPhoto_Tests.swift
+//  Doaaalageel_Lab11_Workshop01_DisplayPhoto*Tests
+//
+//  Created by Dua'a ageel on 14/03/1443 AH.
+//
+
+import XCTest
+@testable import Doaaalageel_Lab11_Workshop01_DisplayPhoto_
+
+class Doaaalageel_Lab11_Workshop01_DisplayPhoto_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

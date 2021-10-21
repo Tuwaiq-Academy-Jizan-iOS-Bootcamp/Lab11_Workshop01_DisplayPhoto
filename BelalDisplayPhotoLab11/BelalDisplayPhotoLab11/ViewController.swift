8fd3336b7d5b1d0e688206a72e403d15588e2330
Belal <mr_belal@Belals-MacBook-Air.local>
2021-10-21 15:12:42 +0300

    Update

--- BelalDisplayPhotoLab11/BelalDisplayPhotoLab11/ViewController.swift
+++ BelalDisplayPhotoLab11/BelalDisplayPhotoLab11/ViewController.swift
@@ -2,7 +2,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-           //Variables\\
+                 //=======\\
+                //Variables\\
+               //===========\\
     var imageLable = ["Dice face 1","Dice Face 2","Dice face 3","Dice face 4","Dice face 5","Dice face 6"]
     var imageArray = ["face1","face2","face3","face4","face5","face6"]
     var number = 0
@@ -10,15 +12,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nextImage: UIButton!
     @IBOutlet weak var backImage: UIButton!
-             //Starter\\
+                  //=====\\
+                 //Starter\\
+                //=========\\
     override func viewDidLoad() {
         super.viewDidLoad()
         number = 1
         imageView.image = UIImage(named: imageArray[0])
         nameOfImage.text = imageLable[0]
-        backImage.isEnabled = false
+        
     }
-           //Button Actions\\
+               //============\\
+              //Button Actions\\
+             //================\\
     @IBAction func buttonRight(_ sender: UIButton) {
         number += 1
         Display()
@@ -27,28 +33,38 @@ class ViewController: UIViewController {
         number -= 1
         Display()
     }
-           //FUnction\\
+                  //======\\
+                 //Function\\
+                //==========\\
     func Display() {
     switch number {
-    case 1: backImage.isEnabled = false
+    case 1:
         imageView.image = UIImage(named: imageArray[0])
         nameOfImage.text = imageLable[0]
-    case 2:backImage.isEnabled = true
+    case 2:
         imageView.image = UIImage(named: imageArray[1])
         nameOfImage.text = imageLable[1]
-    case 3: backImage.isEnabled = true
+    case 3:
         imageView.image = UIImage(named: imageArray[2])
         nameOfImage.text = imageLable[2]
-    case 4: backImage.isEnabled = true
+    case 4:
         imageView.image = UIImage(named: imageArray[3])
         nameOfImage.text = imageLable[3]
-    case 5: nextImage.isEnabled = true
+    case 5:
         imageView.image = UIImage(named: imageArray[4])
         nameOfImage.text = imageLable[4]
-    case 6: nextImage.isEnabled = false
+    case 6:
         imageView.image = UIImage(named: imageArray[5])
         nameOfImage.text = imageLable[5]
-    default: number = 0
+    case 7:
+        number = 1
+        imageView.image = UIImage(named: imageArray[0])
+        nameOfImage.text = imageLable[0]
+    case 0:
+        number = 6
+        imageView.image = UIImage(named: imageArray[5])
+        nameOfImage.text = imageLable[5]
+    default: number = 1
     }
     }
 

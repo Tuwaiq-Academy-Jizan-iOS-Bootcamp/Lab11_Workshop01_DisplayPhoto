5e1b120e9d4d3b3ada07483ff1e6e07efb1f9241
noura098 <noura.alnami.jiz.s@tuwaiq.edu.sa>
2021-10-22 00:32:39 +0300

    lab11

---
+++ NouraAlnami/NouraAlnami/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  NouraAlnami
+//
+//  Created by NoON .. on 14/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageName: UILabel!
+    
+    @IBOutlet weak var images: UIImageView!
+    
+    var index: Int = 0
+    var arrayImages1 = ["Eiffel Tower","Funa Square","statue of Liberty"]
+    
+    
+    @IBAction func buttonOne(_ sender: Any) {
+    
+    if index < arrayImages1.count - 1 {
+            index += 1
+        } else {
+            index = 0
+        }
+        images.image = UIImage(named: arrayImages1[index] )
+        imageName.text = arrayImages1[index]
+    }
+        
+    @IBAction func buttonTow(_ sender: Any) {
+        if index > 0 {
+            index -= 1
+        } else {
+            index = arrayImages1.count - 1
+        }
+        images.image = UIImage(named: arrayImages1[index] )
+        imageName.text = arrayImages1[index]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+images.image = UIImage(named: arrayImages1[index])
+imageName.text = arrayImages1[index]
+}
+
+}

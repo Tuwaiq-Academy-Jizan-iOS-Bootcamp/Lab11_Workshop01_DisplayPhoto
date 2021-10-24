6e6a7412617c3942548f04079b5a9f0a74431c69
MUSAMASMALI <musa14077@yahoo.com>
2021-10-24 11:05:07 +0300

    musa

---
+++ Display photo ./Display photo .UITests/Display_photo__UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Display_photo__UITestsLaunchTests.swift
+//  Display photo .UITests
+//
+//  Created by موسى مسملي on 23/10/2021.
+//
+
+import XCTest
+
+class Display_photo__UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
